8c0ef461e5959b504dd430ea6e6c10853c209f31
Rakshit <rakshit087@gmail.com>
2025-03-01 20:00:52 +0530

    fix: added status in sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,7 @@
-CREATE TABLE jobs (
+CREATE TABLE IF NOT EXISTS jobs (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     status TEXT CHECK (status IN ('pending', 'processing', 'completed', 'failed')) DEFAULT 'pending',
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    type TEXT NOT NULL
 );
