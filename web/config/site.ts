5878456f93f798a970f4487c28fb16e34aaac468
Rakshit <rakshit087@gmail.com>
2025-03-02 23:00:44 +0530

    jobs

--- web/config/site.ts
+++ web/config/site.ts
@@ -7,12 +7,12 @@ export const siteConfig = {
   mainNav: [
     {
       title: "Jobs",
-      href: "/",
+      href: "/jobs",
     },
   ],
   links: {
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn/ui",
-    docs: "https://ui.shadcn.com",
+    twitter: "/",
+    github: "/",
+    docs: "/",
   },
 }
